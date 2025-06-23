c22bca76c0e6284344406cd03b1e6915138c2280
John Akridge <John.akridge92@gmail.com>
2025-06-23 16:39:15 -0400

    Fix dayMatch

--- index.js
+++ index.js
@@ -458,14 +458,13 @@ cron.schedule('* * * * *', async () => {
       
 
       const timeMatch = user.time_of_day.slice(0, 5) === currentTime;
-      console.log('Time match is:', timeMatch);
-      console.log('Match?', timeMatch, dayMatch);
-
       const dayMatch = (
         user.frequency === 'daily' ||
         (user.frequency === 'weekly' && now.format('dddd') === user.day_of_week) ||
         (user.frequency === 'monthly' && now.date() === user.day_of_month)
       );
+      console.log('Time match is:', timeMatch);
+      console.log('Match?', timeMatch, dayMatch);
 
       if (timeMatch && dayMatch) {
         const [properties] = await db.promise().query(
