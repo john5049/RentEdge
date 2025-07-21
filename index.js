0062399cc2537a7c1a9948f9ffa33000250d9ea9
John Akridge <John.akridge92@gmail.com>
2025-07-21 11:12:43 -0400

    Make it a promise call

--- index.js
+++ index.js
@@ -592,7 +592,7 @@ app.get('/api/reporting/get', async (req, res) => {
   }
 
   try {
-    const [rows] = await pool.query(
+    const [rows] = await pool.promise().query(
       `SELECT frequency, day_of_week, day_of_month, time_of_day, recipients
        FROM report_schedules
        WHERE user_id = ? LIMIT 1`,
